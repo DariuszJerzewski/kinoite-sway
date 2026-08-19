ecbb6ac0d4887fa6499ef9ffb3df5480b0430b3c
DariuszJerzewski <dariuszjerzewski.dj@gmail.com>
2026-08-19 08:19:58 +0200

    updated zsh script and added some pre-configuration

--- files/scripts/setup-zsh.sh
+++ files/scripts/setup-zsh.sh
@@ -3,8 +3,19 @@ set -oue pipefail
 
 useradd -D -s /bin/zsh
 
+# Clone repos
 git clone https://github.com/ohmyzsh/ohmyzsh.git /usr/share/oh-my-zsh
-chmod -R 755 /usr/share/oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh-syntax-highlighting
 
 cp /usr/share/oh-my-zsh/templates/zshrc.zsh-template /etc/skel/.zshrc
-sed -i 's|export ZSH=$HOME/.oh-my-zsh|export ZSH=/usr/share/oh-my-zsh|' /etc/skel/.zshrc
+
+# Use the shared Oh My Zsh installation
+sed -i 's|^export ZSH=.*|export ZSH=/usr/share/oh-my-zsh|' /etc/skel/.zshrc
+
+# Import system-wide plugins
+cat >> /etc/skel/.zshrc <<'EOF'
+
+# Load system-wide Zsh plugins
+source /etc/zsh/plugins.zsh
+EOF
---
+++ files/system/etc/zsh/plugins.zsh
@@ -0,0 +1,5 @@
+### System-wide Zsh plugins
+# ensures correct ordering of imports no matter of changes in user's ZSH file
+
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
